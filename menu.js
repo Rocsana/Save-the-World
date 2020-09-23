c5fbd54210aecc6f0fbdc7d646f9e1736b6c07d8
Rocsana <rocsana.suteu@gmail.com>
2020-09-23 11:44:59 +0300

    Modificari

--- menu.js
+++ menu.js
@@ -1,13 +1,17 @@
 function openMenu () {
-    document.getElementById("content").classList.toggle( "hamburger--opened");
+    document.getElementById("content").classList.toggle( "hero__hamburger--opened");
 }
 
 function dropMenu () {
-     document.getElementById("minimenu").classList.toggle( "navbar__drop");
+     document.getElementById("minimenu").classList.toggle( "hero__navbar--drop");
 }
 
 const images = ["./Photos/hero-image 2.png", "./Photos/hero-image 3.png", "./Photos/hero-image 4.png"];
 
 function background (index) {
     document.getElementById("backgrimg").style.backgroundImage =`url("${images[index]}")`;
+}
+
+function openHamb () {
+    document.getElementById("larger").classList.toggle("hero__hamburger--minmen");
 }
