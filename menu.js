28c414c38fbc3936b18862ee713a50902028e12f
Rocsana <rocsana.suteu@gmail.com>
2020-09-28 23:20:38 +0300

    Improvements after review

--- menu.js
+++ menu.js
@@ -1,17 +1,23 @@
-function openMenu () {
-    document.getElementById("content").classList.toggle( "hero__hamburger--opened");
-}
+function openMen(x) {
+    var el = document.getElementById("hamb");
+    el.classList.toggle("change");
+    var element = document.getElementById("content");
+    element.classList.toggle("hamburger__opened");
+  }
 
 function dropMenu () {
-     document.getElementById("minimenu").classList.toggle( "hero__navbar--drop");
+    var menu = document.getElementById("minimenu");
+    menu.classList.toggle( "navbar__drop");
 }
 
 const images = ["./Photos/hero-image 2.png", "./Photos/hero-image 3.png", "./Photos/hero-image 4.png"];
 
 function background (index) {
-    document.getElementById("backgrimg").style.backgroundImage =`url("${images[index]}")`;  
+    var background =  document.getElementById("backgrimg");
+    background.style.backgroundImage =`url("${images[index]}")`;  
 }
 
 function openHamb () {
-    document.getElementById("larger").classList.toggle("hero__hamburger--minmen");
+    var menuHamb = document.getElementById("insideMen");
+    menuHamb.classList.toggle("hamburger__minmen");
 }
