49e696ced4375a354eff626f854395faf58c7736
Rocsana <70335210+Rocsana@users.noreply.github.com>
2020-10-01 16:27:05 +0300

    Feature/menu (#5)
    * Menu in progress
    
    * Menuuuuu
    
    * Menu and slidebar in progress
    
    * Menu and slideshow
    
    * Project improvements
    
    * Modificari
    
    * Feature/improvements (#4)
    
    * Project Improvements
    
    * All improvements so far
    
    * Improvements
    
    * Improvements after review
    
    * Improvements after review
    
    Co-authored-by: Adina Badila <48824233+adinabadila@users.noreply.github.com>
    
    Co-authored-by: Adina Badila <48824233+adinabadila@users.noreply.github.com>

--- menu.js
+++ menu.js
@@ -1,13 +1,23 @@
-function openMenu () {
-    document.getElementById("content").classList.toggle( "hamburger--opened");
-}
+function openMen() {
+    var el = document.getElementById("hamb");
+    el.classList.toggle("change");
+    var element = document.getElementById("content");
+    element.classList.toggle("hamburger__opened");
+  }
 
 function dropMenu () {
-     document.getElementById("minimenu").classList.toggle( "navbar__drop");
+    var menu = document.getElementById("minimenu");
+    menu.classList.toggle( "navbar__drop");
 }
 
 const images = ["./Photos/hero-image 2.png", "./Photos/hero-image 3.png", "./Photos/hero-image 4.png"];
 
 function background (index) {
-    document.getElementById("backgrimg").style.backgroundImage =`url("${images[index]}")`;
+    var background =  document.getElementById("backgrimg");
+    background.style.backgroundImage =`url("${images[index]}")`;  
+}
+
+function openHamb () {
+    var menuHamb = document.getElementById("insideMen");
+    menuHamb.classList.toggle("hamburger__minmen");
 }
